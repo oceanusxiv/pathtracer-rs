9248b97ce3be9bc9039978f6b09a82880298e119
Eric Fang <eric1221bday@gmail.com>
2019-08-15 02:07:53 -0700

    split pipeline out to separate file

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -3,6 +3,7 @@
     allow(unused)
 )]
 
+mod pipelines;
 mod shaders;
 
 use rendy::{
@@ -21,6 +22,8 @@ use rendy::{
     wsi::winit::{self, Event, EventsLoop, WindowBuilder, WindowEvent},
 };
 
+use pipelines::TriangleRenderPipeline;
+
 use std::{collections::HashSet, time};
 
 #[cfg(feature = "dx12")]
@@ -34,150 +37,6 @@ pub type Backend = rendy::vulkan::Backend;
 
 #[cfg(feature = "empty")]
 pub type Backend = rendy::empty::Backend;
-
-#[cfg(not(feature = "spirv-reflection"))]
-use rendy::mesh::AsVertex;
-
-#[derive(Debug, Default)]
-struct TriangleRenderPipelineDesc;
-
-#[derive(Debug)]
-struct TriangleRenderPipeline<B: hal::Backend> {
-    vertex: Option<Escape<Buffer<B>>>,
-}
-
-impl<B, T> SimpleGraphicsPipelineDesc<B, T> for TriangleRenderPipelineDesc
-where
-    B: hal::Backend,
-    T: ?Sized,
-{
-    type Pipeline = TriangleRenderPipeline<B>;
-
-    fn depth_stencil(&self) -> Option<hal::pso::DepthStencilDesc> {
-        None
-    }
-
-    fn vertices(
-        &self,
-    ) -> Vec<(
-        Vec<hal::pso::Element<hal::format::Format>>,
-        hal::pso::ElemStride,
-        hal::pso::VertexInputRate,
-    )> {
-        #[cfg(feature = "spirv-reflection")]
-        return vec![SHADER_REFLECTION
-            .attributes_range(..)
-            .unwrap()
-            .gfx_vertex_input_desc(hal::pso::VertexInputRate::Vertex)];
-
-        #[cfg(not(feature = "spirv-reflection"))]
-        return vec![PosColor::vertex().gfx_vertex_input_desc(hal::pso::VertexInputRate::Vertex)];
-    }
-
-    fn load_shader_set(&self, factory: &mut Factory<B>, _aux: &T) -> ShaderSet<B> {
-        shaders::SHADERS.build(factory, Default::default()).unwrap()
-    }
-
-    fn build<'a>(
-        self,
-        _ctx: &GraphContext<B>,
-        _factory: &mut Factory<B>,
-        _queue: QueueId,
-        _aux: &T,
-        buffers: Vec<NodeBuffer>,
-        images: Vec<NodeImage>,
-        set_layouts: &[Handle<DescriptorSetLayout<B>>],
-    ) -> Result<TriangleRenderPipeline<B>, failure::Error> {
-        assert!(buffers.is_empty());
-        assert!(images.is_empty());
-        assert!(set_layouts.is_empty());
-
-        Ok(TriangleRenderPipeline { vertex: None })
-    }
-}
-
-impl<B, T> SimpleGraphicsPipeline<B, T> for TriangleRenderPipeline<B>
-where
-    B: hal::Backend,
-    T: ?Sized,
-{
-    type Desc = TriangleRenderPipelineDesc;
-
-    fn prepare(
-        &mut self,
-        factory: &Factory<B>,
-        _queue: QueueId,
-        _set_layouts: &[Handle<DescriptorSetLayout<B>>],
-        _index: usize,
-        _aux: &T,
-    ) -> PrepareResult {
-        if self.vertex.is_none() {
-            #[cfg(feature = "spirv-reflection")]
-            let vbuf_size = shaders::SHADER_REFLECTION
-                .attributes_range(..)
-                .unwrap()
-                .stride as u64
-                * 3;
-
-            #[cfg(not(feature = "spirv-reflection"))]
-            let vbuf_size = PosColor::vertex().stride as u64 * 3;
-
-            let mut vbuf = factory
-                .create_buffer(
-                    BufferInfo {
-                        size: vbuf_size,
-                        usage: hal::buffer::Usage::VERTEX,
-                    },
-                    Dynamic,
-                )
-                .unwrap();
-
-            unsafe {
-                // Fresh buffer.
-                factory
-                    .upload_visible_buffer(
-                        &mut vbuf,
-                        0,
-                        &[
-                            PosColor {
-                                position: [0.0, -0.5, 0.0].into(),
-                                color: [1.0, 0.0, 0.0, 1.0].into(),
-                            },
-                            PosColor {
-                                position: [0.5, 0.5, 0.0].into(),
-                                color: [0.0, 1.0, 0.0, 1.0].into(),
-                            },
-                            PosColor {
-                                position: [-0.5, 0.5, 0.0].into(),
-                                color: [0.0, 0.0, 1.0, 1.0].into(),
-                            },
-                        ],
-                    )
-                    .unwrap();
-            }
-
-            self.vertex = Some(vbuf);
-        }
-
-        PrepareResult::DrawReuse
-    }
-
-    fn draw(
-        &mut self,
-        _layout: &B::PipelineLayout,
-        mut encoder: RenderPassEncoder<'_, B>,
-        _index: usize,
-        _aux: &T,
-    ) {
-        let vbuf = self.vertex.as_ref().unwrap();
-        unsafe {
-            encoder.bind_vertex_buffers(0, Some((vbuf.raw(), 0)));
-            encoder.draw(0..3, 0..1);
-        }
-    }
-
-    fn dispose(self, _factory: &mut Factory<B>, _aux: &T) {}
-}
 
 #[cfg(any(feature = "dx12", feature = "metal", feature = "vulkan"))]
 fn main_loop(
