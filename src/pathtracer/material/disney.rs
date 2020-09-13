d5bb58b7f1618c5628943dc6d3164a4f3b968e31
Eric Fang <eric1221bday@gmail.com>
2020-09-13 11:58:53 -0700

    disney material struct def

--- src/pathtracer/material/disney.rs
+++ src/pathtracer/material/disney.rs
@@ -1,1 +1,15 @@
+use crate::{common::spectrum::Spectrum, pathtracer::texture::SyncTexture};
 
+pub struct DisneyMaterial {
+    color: Box<dyn SyncTexture<Spectrum>>,
+    metallic: Box<dyn SyncTexture<f32>>,
+    eta: Box<dyn SyncTexture<f32>>,
+    roughness: Box<dyn SyncTexture<f32>>,
+    normal_map: Option<Box<dyn SyncTexture<na::Vector3<f32>>>>,
+}
+
+impl DisneyMaterial {
+    // pub fn new() -> Self {
+
+    // }
+}
