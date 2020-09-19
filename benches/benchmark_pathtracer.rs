151b3e76a0ad842f6a523de3b19401b4b6e469c1
Eric Fang <eric1221bday@gmail.com>
2020-09-19 07:48:48 -0700

    consolidate benchmark into one file

--- benches/benchmark_pathtracer.rs
+++ benches/benchmark_pathtracer.rs
@@ -4,7 +4,7 @@ extern crate slog;
 use criterion::*;
 use pathtracer_rs::*;
 
-fn bench(c: &mut Criterion) {
+fn bench_render(c: &mut Criterion) {
     let mut group = c.benchmark_group("benchmark-render");
 
     let drain = slog::Discard;
@@ -20,11 +20,32 @@ fn bench(c: &mut Criterion) {
     integrator.preprocess(&render_scene);
 
     group.sampling_mode(SamplingMode::Flat).sample_size(10);
-    group.bench_function("render", |b| {
+    group.bench_function("bench_render", |b| {
         b.iter(|| integrator.render(&mut camera, &render_scene))
     });
     group.finish();
 }
 
-criterion_group!(benches, bench);
+fn bench_bounds(c: &mut Criterion) {
+    let mut group = c.benchmark_group("benchmark-bounds");
+
+    let r = common::ray::Ray {
+        o: nalgebra::Point3::origin(),
+        d: nalgebra::Vector3::new(1.0, 1.0, 1.0),
+        t_max: f32::INFINITY,
+    };
+    let inv_dir = nalgebra::Vector3::new(1.0f32 / r.d.x, 1.0f32 / r.d.y, 1.0f32 / r.d.z);
+    let dir_is_neg = [inv_dir.x < 0.0, inv_dir.y < 0.0, inv_dir.z < 0.0];
+    let bounds = common::bounds::Bounds3 {
+        p_min: nalgebra::Point3::origin(),
+        p_max: nalgebra::Point3::new(1.0, 1.0, 1.0),
+    };
+
+    group.bench_function("bench_bounds", |b| {
+        b.iter(|| bounds.intersect_p_precomp(&r, &inv_dir, &dir_is_neg))
+    });
+    group.finish();
+}
+
+criterion_group!(benches, bench_bounds, bench_render);
 criterion_main!(benches);
